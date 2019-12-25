d795193f5d81d8bd0f6c7f3c1649c624b1cef382
Samed Karakus <karakusnavy@gmail.com>
2019-12-25 18:06:27 +0300

    Uygulamayı sıfırladım kendime göre ayar çektim
    shared page sistemini yaptım şuanda tasarıma geçiş yapacağım

---
+++ pages/SharedPages/Footer.js
@@ -0,0 +1,16 @@
+
+const Footer = () => {
+    return (
+        <div>
+            <h1>Footer</h1>
+            <style jsx>{`
+            h1{
+                color:red
+            }
+            `}
+            </style>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ pages/SharedPages/Header.js
@@ -0,0 +1,16 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Header</h1>
+            <style jsx>{`
+            h1{
+                color:red
+            }
+            `}
+            </style>
+        </div>
+    )
+}
+
+export default Header;
