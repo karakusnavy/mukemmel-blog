418bf1ad9266b15d35e29221a8959734888fdc54
Samed Karakus <karakusnavy@gmail.com>
2019-12-31 15:09:16 +0300

    Update dashboard.js

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -20,57 +20,7 @@ export default class dashboard extends React.Component {
                 <link href="../admin/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet" />
                 <link href="../admin/css/sb-admin.css" rel="stylesheet" />
                 <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
-                    <a class="navbar-brand mr-1" href="index.html">Start Bootstrap</a>
-                    <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
-                        <i class="fas fa-bars"></i>
-                    </button>
-                    <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
-                        <div class="input-group">
-                            <input type="text" class="form-control" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2" />
-                            <div class="input-group-append">
-                                <button class="btn btn-primary" type="button">
-                                    <i class="fas fa-search"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                    <ul class="navbar-nav ml-auto ml-md-0">
-                        <li class="nav-item dropdown no-arrow mx-1">
-                            <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-bell fa-fw"></i>
-                                <span class="badge badge-danger">9+</span>
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="alertsDropdown">
-                                <a class="dropdown-item" href="#">Action</a>
-                                <a class="dropdown-item" href="#">Another action</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#">Something else here</a>
-                            </div>
-                        </li>
-                        <li class="nav-item dropdown no-arrow mx-1">
-                            <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-envelope fa-fw"></i>
-                                <span class="badge badge-danger">7</span>
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="messagesDropdown">
-                                <a class="dropdown-item" href="#">Action</a>
-                                <a class="dropdown-item" href="#">Another action</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#">Something else here</a>
-                            </div>
-                        </li>
-                        <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-user-circle fa-fw"></i>
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#">Settings</a>
-                                <a class="dropdown-item" href="#">Activity Log</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
-                            </div>
-                        </li>
-                    </ul>
+                    <a class="navbar-brand mr-1" href="index.html">Samed Karakuş - Admin Paneli</a>
                 </nav>
                 <div id="wrapper">
                     <div id="content-wrapper">
@@ -102,13 +52,16 @@ export default class dashboard extends React.Component {
                                         </div>
                                     </div>
                                 </div>
-
+                                <div class="col-xl-6 col-sm-6 mb-6">
+                                <button type="button" class="btn btn-success">Yeni Blog Ekle</button>
+                                <button style={{marginLeft:10}} type="button" class="btn btn-success">Yeni Kullanıcı Ekle</button>
+                                </div>
+                                
                             </div>
-
                             <div class="card mb-3">
                                 <div class="card-header">
                                     <i class="fas fa-table"></i>
-                                    Blog Yazılarım</div>                                    
+                                    Blog Yazılarım</div>
                                 <div class="card-body">
                                     <div class="table-responsive">
                                         <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
@@ -145,59 +98,20 @@ export default class dashboard extends React.Component {
                                         </table>
                                     </div>
                                 </div>
-                                <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
-                            </div>
-
-                        </div>
-
-
-
-                        <footer class="sticky-footer">
-                            <div class="container my-auto">
-                                <div class="copyright text-center my-auto">
-                                    <span>Copyright © Bootstrap Tasarımı - Samed Karakuş 2019</span>
-                                </div>
-                            </div>
-                        </footer>
-
-                    </div>
-                </div>
-                <a class="scroll-to-top rounded" href="#page-top">
-                    <i class="fas fa-angle-up"></i>
-                </a>
-                <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">×</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                            <div class="modal-footer">
-                                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                                <a class="btn btn-primary" href="login.html">Logout</a>
+                                <div class="card-footer small text-muted">Son yazı eklenme tarihi : 09.09.2019</div>
                             </div>
                         </div>
                     </div>
                 </div>
-
                 <script src="../admin/vendor/jquery/jquery.min.js"></script>
                 <script src="../admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
                 <script src="../admin/vendor/jquery-easing/jquery.easing.min.js"></script>
-
                 <script src="../admin/vendor/chart.js/Chart.min.js"></script>
                 <script src="../admin/vendor/datatables/jquery.dataTables.js"></script>
                 <script src="../admin/vendor/datatables/dataTables.bootstrap4.js"></script>
-
                 <script src="../admin/js/sb-admin.min.js"></script>
-
                 <script src="../admin/js/demo/datatables-demo.js"></script>
                 <script src="../admin/js/demo/chart-area-demo.js"></script>
-
-
             </div>
         )
     }
