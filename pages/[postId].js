1feb20b033ea4686fa23d839e78369fffdf13f19
Samed Karakus <karakusnavy@gmail.com>
2020-01-08 16:44:57 +0300

    sayfalama yapıları falan derken react next react native her şeyi birbirine karıştırmışım sıfırdan başlıyorum düzenli olarak

--- pages/[postId].js
+++ pages/[postId].js
@@ -4,7 +4,7 @@ import Head from "next/head";
 import Link from "next/link";
 import ReactMarkdown from "react-markdown";
 
-const Home = ({ posts }) => (
+const BlogPost = ({ post }) => (
   <div className="container">
     <Head>
       <title>Home</title>
@@ -29,20 +29,17 @@ const Home = ({ posts }) => (
       </div>
     </div>
 
-    {posts.map(post => (
-      <div className="blog">
-        <h2 className="blog-title">
-          <Link href={post.slug}>
-            <a className="blog-title-link">{post.title}</a>
-          </Link>
-        </h2>
-        <div className="blog-text">
-          <ReactMarkdown source={post.details} />
-        </div>
-        <div className="blog-date">{post.date}</div>
+    <div className="blog">
+      <h2 className="blog-title">
+        <Link href="/test">
+          <a className="blog-title-link">{post.title}</a>
+        </Link>
+      </h2>
+      <div className="blog-text">
+        <ReactMarkdown source={post.details} />
       </div>
-    ))}
-
+      <div className="blog-date">{post.date}</div>
+    </div>
     <style jsx>{`
       .container {
         max-width: 650px;
@@ -77,11 +74,11 @@ const Home = ({ posts }) => (
   </div>
 );
 
-Home.getInitialProps = async ({ req }) => {
+BlogPost.getInitialProps = async ({ req, query }) => {
   // TODO: aşağıdaki satırda bulunan adresi kendi sunucu adresinle değiştirmelisin
-  const res = await fetch("http://localhost:3000/api/posts");
+  const res = await fetch(`http://localhost:3000/api/post/${query.postId}`);
   const json = await res.json();
-  return { posts: json.posts };
+  return { post: json.post };
 };
 
-export default Home;
+export default BlogPost;
