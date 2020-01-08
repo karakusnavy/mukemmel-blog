639d9269d0ccf923e4f338a130e670248515ea44
Samed Karakus <karakusnavy@gmail.com>
2020-01-08 17:35:31 +0300

    oha next js harika :D

--- pages/[postId].js
+++ pages/[postId].js
@@ -1,19 +1,34 @@
 import React from "react";
+import firebase from 'firebase'
+import firebaseconnection from '../components/firebaseconnection'
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseconnection);
+}
 
-const posts = ({ query }) =>{
-  return(
+const posts = ({ query }) => {
+  return (
     <div>
-      {query}
+      {query[0].title}
     </div>
   )
 }
 
 posts.getInitialProps = async ({ req2, query }) => {
-  
-  
-  
- return {query: query.postId};
- 
+  var BlogText = []
+  firebase.database().ref().child('blogs').on('child_added', data => {
+    if (data.val().link == query.postId) {
+
+      BlogText.push({
+        title: data.val().title,
+        image: data.val().image,
+        blog: data.val().blog,
+      })
+    }
+  })
+
+
+  return { query: BlogText };
+
 };
 
 export default posts;
